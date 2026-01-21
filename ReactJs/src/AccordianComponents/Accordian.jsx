317ad9dc1c29201caea0ee32791159fa30b67eac
Omkar Shelke <shelkeomkar272@gmail.com>
2026-01-21 11:58:18 +0530

    Accordian Completed

---
+++ ReactJs/src/AccordianComponents/Accordian.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import "./style.css";
+import data from "./data";
+
+function Accordian() {
+  const [selectedItem, setSelectedItem] = useState(null);
+  const [multiselectionMode , setMultiSelectionMode] = useState(false);
+  const [multipleSelectedItems , setMultipleSelectedItems] = useState([]);
+
+
+  // Single Select
+  function handleSingleSelect(currId) {
+    setSelectedItem(currId === selectedItem? null: currId); 
+  }
+
+  function handleMultiSelect(currId) {
+    let copyarr = [...multipleSelectedItems];
+    const idx = copyarr.indexOf(currId);
+    
+    if(idx === -1) copyarr.push(currId)
+    else copyarr.splice(idx, 1);
+
+    setMultipleSelectedItems(copyarr);
+  }
+
+  return (
+    <div>
+      <div className="wrapper">
+        <button className="enableBtn" onClick={() => setMultiSelectionMode(!multiselectionMode)}>{multiselectionMode === true? <span>Disable</span> : <span>Enable</span>} multiple selection</button>
+        <div className="container">
+          {data && data.length > 0 ? (
+            data.map((dataItem, idx) => (
+              <div
+                className="Item"
+                key={idx}
+                onClick={multiselectionMode === true? () => handleMultiSelect(dataItem.id) : () => handleSingleSelect(dataItem.id)}
+              >
+                <div className="title">
+                  <h2> {dataItem.question} </h2>
+                  <span>+</span>
+                </div>
+                {multiselectionMode === true? multipleSelectedItems.includes(dataItem.id)? <div> {dataItem.answer} </div> : null : selectedItem === dataItem.id ? <div> {dataItem.answer} </div> : null}
+                
+              </div>
+            ))
+          ) : (
+            <div> Data Not Found </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Accordian;
---
+++ ReactJs/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Accordian from './AccordianComponents/Accordian'
+
+function App() {
+  return (
+    <>
+      <Accordian />
+    </>
+  )
+}
+
+export default App
