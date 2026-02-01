8e3276c7aaa51499b18faf665243411c3cd774b2
Omkar Shelke <shelkeomkar272@gmail.com>
2026-02-01 11:56:29 +0530

    Custom Scroll Indicator Completed

--- ReactJs/src/CustomScrollIndicator/ScrollIndicator.jsx
+++ ReactJs/src/CustomScrollIndicator/ScrollIndicator.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { BeatLoader } from "react-spinners";
+import "./style.css";
 
 function ScrollIndicator({ url }) {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [scrollPercent, setScrollPercent] = useState(0);
 
   async function fetchData(getUrl) {
     try {
@@ -23,15 +25,27 @@ function ScrollIndicator({ url }) {
     (async () => await fetchData(url))();
   }, [url]);
 
+  function handleScroll(e) {
+    const scrollTop = e.target.scrollTop; //  Total Scroll From the top
+
+    const scrollHeight = e.target.scrollHeight;
+    const clientHeight = e.target.clientHeight;
+    const maxScroll = scrollHeight - clientHeight; // Total Height of scroll
+    setScrollPercent((scrollTop / maxScroll) * 100);
+    // console.log(scrollPercent);
+  }
+
   return (
-    <div className="scrollContainer">
+    <div className="scrollContainer" onScroll={handleScroll}>
       {loading ? (
         <BeatLoader />
       ) : (
         <div>
-          {" "}
-          <h2>Custom Scroll Indicator</h2>
-          <div>
+          <div className="scrollHeader">
+            <h2>Custom Scroll Indicator</h2>
+            <div style={{width: `${scrollPercent}%` , backgroundColor: "#FFDAB3" , height: "10px"}}></div>
+          </div>
+          <div className="ScrollContent">
             {data.map((product) => {
               return (
                 <div key={product.id}>
